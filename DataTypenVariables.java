2bc0894c8963cd90ddb6404b4f49b4059a7d19ca
sharan <sharannandakumar2k4@gmail.com>
2026-01-24 14:04:25 +0530

    Add Day 1 Java programs

---
+++ DataTypenVariables.java
@@ -0,0 +1,14 @@
+public class DataTypenVariables{
+    public static void main(String[]args){
+      int a = -1;
+ if (a > 0){
+    System.out.println("integer is positive");
+ }else if(a==0){
+    System.out.println("integer is NULL");
+ }else{
+    System.out.println("integer is Negative");
+ }
+
+    }
+
+}
